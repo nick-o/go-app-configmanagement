9cd64e8f01e8d90d4916fe27b68cc1cbb908f5a7
Nico Engelen <engelen.nico@googlemail.com>
2016-07-27 22:28:35 +0100

    pushing files generated by berks

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'go-app-configmanagment'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures go-app-configmanagment'
+long_description 'Installs/Configures go-app-configmanagment'
+version          '0.1.0'
